40728fab84c2ef0798cbac969fd20f427c4f356f
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-02-08 11:44:58 -0600

    initialize project structure with basic configurations and resources

--- app/app/src/main/java/com/app/twenty_apps/utils/Fonts.kt
+++ app/app/src/main/java/com/app/twenty_apps/utils/Fonts.kt
@@ -1,4 +1,16 @@
 package com.app.twenty_apps.utils
 
-class Fonts {
-}
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.app.twenty_apps.R
+
+val morganite = FontFamily(
+  Font(R.font.morganite700, FontWeight.Bold),
+  Font(R.font.morganite800, FontWeight.ExtraBold)
+)
+val openSauce = FontFamily(
+  Font(R.font.opensauce400, FontWeight.Normal),
+  Font(R.font.opensauce700, FontWeight.Bold),
+  Font(R.font.opensauce800, FontWeight.ExtraBold)
+)
