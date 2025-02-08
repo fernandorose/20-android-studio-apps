4f1de2d5608e7b3b7a26c090d94fe7c55201c371
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-02-08 11:54:35 -0600

    Create chronometer app screen

---
+++ app/app/src/main/java/com/app/twenty_apps/screens/ChronometerScreen.kt
@@ -0,0 +1,9 @@
+package com.app.twenty_apps.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ChronometerScreen(navController: NavController){
+
+}
