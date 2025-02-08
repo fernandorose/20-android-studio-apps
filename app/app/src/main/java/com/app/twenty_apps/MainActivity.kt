cd332f5203bd3004a428a22100ef02175137152b
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-02-08 11:42:20 -0600

    implement MainActivity with edge-to-edge support and navigation setup

--- app/app/src/main/java/com/app/twenty_apps/MainActivity.kt
+++ app/app/src/main/java/com/app/twenty_apps/MainActivity.kt
@@ -1,4 +1,20 @@
 package com.app.twenty_apps
 
-class Main {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.Color
+import com.app.twenty_apps.screens.MenuScreen
+import com.app.twenty_apps.utils.AppNavigation
+import com.app.twenty_apps.utils.ChangeStatusBarColor
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      AppNavigation()
+    }
+  }
 }
